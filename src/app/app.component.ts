d22b620930301927bb082334d16845f1f2402069
SBereno <percival1999@gmail.com>
2021-03-08 00:43:48 +0100

    ADD funcionalidad botón regiones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PokemonApiService } from './Pokeapi.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Pokenerator';
+export class AppComponent implements OnInit {
+  regionsArray;
+  constructor(private _pokemonApiService: PokemonApiService) {
+  }
+
+  ngOnInit() {
+    this._pokemonApiService.getRegions()
+    .subscribe(response => {
+      this.regionsArray = response.results;
+    });
+  }
+
+  onSubmit() {
+    for (let i = 0; i < this.regionsArray.length; i++) {
+      console.log(this.regionsArray[i].name);
+    }
+  }
+
+  selectRegion(newRegion) {
+    document.getElementById("regiones").innerText = newRegion.charAt(0).toUpperCase() + newRegion.substr(1).toLowerCase();
+  }
 }
